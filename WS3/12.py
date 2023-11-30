3abf6b1f2b7e5921dc4e181137185a72471d7587
YoshiFromBi <>
2023-11-30 17:32:11 +0100

    Starting the ex12

--- WS3/12.py
+++ WS3/12.py
@@ -1,1 +1,22 @@
-print("Hello world")
+# Write an algorithm that given  two lists by the user says  if they have a  common element or not.
+
+#We're using ListInput.py
+
+
+size=input("Give a size:\n")
+newList1=[]
+
+for i in range(int(size)):
+    element=input("Give an element to push into the list:\n")
+    newList1.append(element)
+
+print(f"Here is the first list! {newList1}")
+
+size=input("Give a size:\n")
+newList2=[]
+
+for i in range(int(size)):
+    element=input("Give an element to push into the list:\n")
+    newList2.append(element)
+
+print(f"Here is the first list! {newList2}")
