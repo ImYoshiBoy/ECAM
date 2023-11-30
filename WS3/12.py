4787a0a8a7fd4eed82883db14429262f5e04788e
YoshiFromBi <>
2023-11-30 17:45:35 +0100

    Finishing ex12

--- WS3/12.py
+++ WS3/12.py
@@ -3,20 +3,37 @@
 #We're using ListInput.py
 
 
-size=input("Give a size:\n")
+size1=input("Give a size:\n")
 newList1=[]
 
-for i in range(int(size)):
+for i in range(int(size1)):
     element=input("Give an element to push into the list:\n")
     newList1.append(element)
 
 print(f"Here is the first list! {newList1}")
 
-size=input("Give a size:\n")
+size2=input("Give a size:\n")
 newList2=[]
 
-for i in range(int(size)):
+for i in range(int(size2)):
     element=input("Give an element to push into the list:\n")
     newList2.append(element)
 
 print(f"Here is the first list! {newList2}")
+
+#Now we want to check if lists have a common element (by creating a flag)
+
+flag=False
+
+for elementList1 in newList1:
+    for elementList2 in newList2:
+        if(elementList1 == elementList2): 
+            flag=True
+            break # Stop the inner loop once a common element is found
+    if flag:
+        break # Stop the outer loop once a common element is found
+
+if flag:
+    print("Common elements")
+else:
+    print("No common element")
