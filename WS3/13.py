011e1c24cb62da6411441233aac52860b9229b49
otpyoshi <hugopommier2008@gmail.com>
2023-12-01 16:00:14 +0100

    Updated 13.py

--- WS3/13.py
+++ WS3/13.py
@@ -12,7 +12,5 @@ for elementList1 in newList1:
     for elementList2 in newList2:
         if(elementList1 == elementList2): 
             i+=1
-        else:
-            i+=0
 
 print(f"{i} common element(s)")
