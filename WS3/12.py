9b808564efc8fbd1a86075d0e8aa140f7c08f58a
otpyoshi <hugopommier2008@gmail.com>
2023-12-01 16:23:18 +0100

    12.py done, 13 done, 14 done

--- WS3/12.py
+++ WS3/12.py
@@ -8,18 +8,14 @@ newList1=askList()
 newList2=askList()
 
 #Now we want to check if lists have a common element (by creating a flag)
-
 flag=False
+i = 0
+j = 0
 
-for elementList1 in newList1:
-    for elementList2 in newList2:
-        if(elementList1 == elementList2): 
+while i < len(newList1) and not flag:
+    while j<len(newList2) and not flag:
+        if newList1[i] == newList2[j]:
             flag=True
-            break # Stop the inner loop once a common element is found
-    if flag:
-        break # Stop the outer loop once a common element is found
-
-if flag:
-    print("Common elements")
-else:
-    print("No common element")
+            print(f"COMONON ELEMENTS : {newList1[i]}")
+        j += 1
+    i += 1
