ae56f8b6ab9f8a61571fe91a856fc2bc8365b6fd
YoshiFromBi <hugopommier2008@gmail.com>
2023-12-06 22:22:39 +0100

    NO MORE LAB AHAHAHAHA

--- lab/test.py
+++
@@ -1,8 +0,0 @@
-from hello import *
-from maths1 import *
-from vowel_counter import *
-from palindrome import *
-from palindrome2 import *
-from palindrome3 import *
-
-print(is_palindrome("CACA"))
