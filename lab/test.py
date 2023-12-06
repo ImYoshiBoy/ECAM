da06201880990f729bbce4a5734d7d88c78cf9b3
YoshiFromBi <hugopommier2008@gmail.com>
2023-12-06 22:16:59 +0100

    Imagine i just released the whole. LAB?

---
+++ lab/test.py
@@ -0,0 +1,8 @@
+from hello import *
+from maths1 import *
+from vowel_counter import *
+from palindrome import *
+from palindrome2 import *
+from palindrome3 import *
+
+print(sentence_palindrome("IN GIRUM IMUS NOCTE ET CONSUIMUR IGNI"))
