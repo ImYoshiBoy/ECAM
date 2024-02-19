936f563ba0f2f4b3a4a3c440046600ec7b0b9517
otpyoshi <hugopommier2008@gmail.com>
2024-02-19 11:54:06 +0100

    Update ws1.py

--- S2/sorting/ws1.py
+++ S2/sorting/ws1.py
@@ -5,6 +5,13 @@ def minimum(any_list:list)->list:
             mini = any_list[i]
     return mini
 
+def maximum(any_list:list)->list:
+    max = any_list[0]
+    for i in range(1, len(any_list)):
+        if max < any_list[i]:
+            max = any_list[i]
+    return max
+
 def rough_sort(int_list:list) -> list:
     sorted_list = []
     for i in range(len(int_list)):
@@ -23,11 +30,23 @@ def bubblesort(any_list:list)->list:
     return any_list
 
 #THIS IS NOW THE WORKSHEET
-def selection_sort(int_list:list)->list:
+
+#Selection sort with the minimum
+def selection_sort_min(int_list:list)->list:
 
     for i in range(len(int_list)):
         mini = minimum(int_list[i:])
         int_list.remove(mini)
         int_list.insert(i, mini)
+
+    return int_list
+
+def selection_sort_max(int_list:list)->list:
+
+    n=len(int_list)
+    for i in range(len(int_list)):
+        maxi = maximum(int_list[:n-i])
+        int_list.remove(maxi)
+        int_list.insert(n-i-1, maxi)
 
     return int_list
