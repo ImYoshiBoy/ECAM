23ec9e0d2dec39f566f871231b4d3fadf1621efd
otpyoshi <hugopommier2008@gmail.com>
2024-02-23 16:21:19 +0100

    Update worksheet.py

--- S2/matplotlib/worksheet.py
+++ S2/matplotlib/worksheet.py
@@ -8,3 +8,9 @@ x = np.linspace(-np.pi / 2, np.pi / 2, 100)
 y = np.sin(x)
 
 plt.plot(x, y)
+
+plt.grid()
+plt.title("sin(x) bewteen -pi/2 and pi/2")
+plt.xlabel("x")
+plt.ylabel("sin(x)")
+plt.show()
